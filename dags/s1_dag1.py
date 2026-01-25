f934f48e1a653d9b968c9836a124e31b2543d494
dan <mai.konovalov.daniil@gmail.com>
2026-01-25 22:40:06 +0300

    исправлена загрузка, сохранение внешних файлов и добавлен полноценный cleanup

--- dags/s1_dag1.py
+++ dags/s1_dag1.py
@@ -1,3 +1,5 @@
+import shutil
+
 import pendulum
 import os
 import zipfile
@@ -15,35 +17,19 @@ BUCKET_NAME_TARGET = "123321test"
 CSV_RE = re.compile(r"\d{6}-citibike-tripdata(_\d+)?\.csv$")
 
 
-def check_if_month_already_loaded(file_name, hook) -> bool:
-    """Check if target month data already loaded"""
+def check_if_month_exists(file_name, hook, bucket_name) -> bool:
+    """Check if target month data exists in bucket"""
     s3_hook = hook
-    response = s3_hook.list_keys(bucket_name=BUCKET_NAME_TARGET)
+    response = s3_hook.check_for_key(key=file_name, bucket_name=bucket_name)
 
-    print(response)
-    if file_name in response:
-        return True
-    else:
-        return False
-
-def check_if_month_exists(file_name, hook) -> bool:
-    """Check if target month data exists"""
-    s3_hook = hook
-    response = s3_hook.list_keys(bucket_name=BUCKET_NAME_SOURCE)
-
-    print(response)
-    if file_name in response:
-        return True
-    else:
-        raise AirflowSkipException(f"Файл {file_name} не найден в бакете!")
-
+    return response
 
 
 default_args = {
     'group': 'g4',
 	'owner': 'Daniil Konovalov',
+    'retries': 3,
     'catchup' : True,
-    'max_active_runs': 1,
 }
 
 @dag(
@@ -65,11 +51,12 @@ def xz():
 
         s3_hook_yandex = S3Hook(aws_conn_id="yandex_s3")
 
-        check_if_month_exists(file_name, s3_hook_yandex)
+        check_if_month_exists(file_name, s3_hook_yandex, BUCKET_NAME_SOURCE)
 
         downloaded_path = s3_hook_yandex.download_file(
             key=file_name,
-            bucket_name="citibike-data",
+            bucket_name=BUCKET_NAME_SOURCE,
+            local_path=SHARED_BASE_DIR,
         )
         print(execution_date[:6])
         return {
@@ -93,47 +80,55 @@ def xz():
         with zipfile.ZipFile(zip_path, "r") as z:
             z.extractall(extract_dir)
 
+        return extract_dir
+
+    @task()
+    def load(extract_dir):
         csv_files = []
+
         for root, _, files in os.walk(extract_dir):
             for f in files:
                 if CSV_RE.match(f):
                     csv_files.append(os.path.join(root, f))
 
-        return csv_files
-
-    @task()
-    def load(csv_files):
-        s3_hook = S3Hook(aws_conn_id='minio')
+        s3_hook_target = S3Hook(aws_conn_id='minio')
         context = get_current_context()
         execution_date = context["ds_nodash"][:6]
         keys = []
-        wrong_keys = []
 
         for idx, file in enumerate(sorted(csv_files)):
             part = f"{idx:02d}"
             key = f"raw/citibike_data/{execution_date}/{execution_date}-citibike-tripdata-part{part}.csv"
-            if check_if_month_already_loaded(key, s3_hook):
-                wrong_keys.append(key)
+
+            if check_if_month_exists(key, s3_hook_target, BUCKET_NAME_TARGET):
                 print(f"Файл {file} уже загружен")
+
             else:
-                s3_hook.load_file(
+                s3_hook_target.load_file(
                     filename=file,
                     key=key,
-                    bucket_name=BUCKET_NAME_TARGET
+                    bucket_name=BUCKET_NAME_TARGET,
                 )
                 keys.append(key)
 
 
                 print(f"Файл успешно загружен: {file} -> {key}")
-        return
 
-    @task()
-    def cleanup(total_order_value: float):
-        return
+    @task(trigger_rule="all_done")
+    def cleanup(data, dir_path):
+        zip_path = data["downloaded_path"]
+        for path in (dir_path, zip_path):
+            if os.path.exists(path):
+                if os.path.isdir(path):
+                    shutil.rmtree(path)
+                else:
+                    os.remove(path)
+                print(f"Удалён {path}")
 
     data = extract()
-    file_list = unzip(data)
-    load(file_list)
-
+    extract_root = unzip(data)
+    load_task = load(extract_root)
+    load_task >> cleanup(data, extract_root)
 
 xz()
+
