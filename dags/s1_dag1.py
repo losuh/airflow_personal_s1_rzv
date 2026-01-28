cc63663c983d99579d342f04b889f604abb8520d
dan <mai.konovalov.daniil@gmail.com>
2026-01-28 15:24:43 +0300

    -

--- dags/s1_dag1.py
+++ dags/s1_dag1.py
@@ -1,4 +1,5 @@
 import shutil
+from logging import raiseExceptions
 
 import pendulum
 import os
@@ -6,9 +7,13 @@ import zipfile
 import re
 
 from airflow.decorators import dag, task
+
 from airflow.operators.python import get_current_context
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.operators.python import PythonOperator
+
+from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
 from airflow.exceptions import AirflowSkipException
 
 SHARED_BASE_DIR = "/opt/airflow/shared_workers_data"
@@ -32,6 +37,91 @@ default_args = {
     'catchup' : True,
 }
 
+@task
+def extract():
+    context = get_current_context()
+    execution_date = context["logical_date"].strftime("%Y%m")
+    file_name = f"{execution_date}-citibike-tripdata.zip"
+
+    s3_hook_yandex = S3Hook(aws_conn_id="yandex_s3")
+
+    if check_if_month_exists(file_name, s3_hook_yandex, BUCKET_NAME_SOURCE):
+
+        downloaded_path = s3_hook_yandex.download_file(
+            key=file_name,
+            bucket_name=BUCKET_NAME_SOURCE,
+            local_path=SHARED_BASE_DIR,
+        )
+
+        print(f"Скачан файл за {execution_date}")
+
+        return {
+            "downloaded_path": downloaded_path,
+            "file_name": file_name,
+        }
+    else:
+        raise AirflowSkipException(f"Нет данных в источнике за дату {execution_date}")
+
+@task()
+def unzip(data):
+    zip_path = data["downloaded_path"]
+    file_name = data["file_name"]
+
+    base_dir = os.path.dirname(zip_path)
+    extract_dir = os.path.join(
+        base_dir,
+        file_name.replace(".zip", "")
+    )
+
+    os.makedirs(extract_dir, exist_ok=True)
+
+    with zipfile.ZipFile(zip_path, "r") as z:
+        z.extractall(extract_dir)
+
+    return extract_dir
+
+@task()
+def load(extract_dir):
+
+    csv_files = []
+
+    for root, _, files in os.walk(extract_dir):
+        for f in files:
+            if CSV_RE.match(f):
+                csv_files.append(os.path.join(root, f))
+
+    s3_hook_target = S3Hook(aws_conn_id='minio')
+    context = get_current_context()
+    execution_date = context["ds_nodash"][:6]
+    keys = []
+
+    for idx, file in enumerate(sorted(csv_files)):
+        part = f"{idx:02d}"
+        key = f"raw/citibike_data/{execution_date}/{execution_date}-citibike-tripdata-part{part}.csv"
+
+        if check_if_month_exists(key, s3_hook_target, BUCKET_NAME_TARGET):
+            print(f"Файл {file} уже загружен")
+        else:
+            s3_hook_target.load_file(
+                filename=file,
+                key=key,
+                bucket_name=BUCKET_NAME_TARGET,
+            )
+            keys.append(key)
+
+            print(f"Файл успешно загружен: {file} -> {key}")
+
+@task(trigger_rule="all_done")
+def cleanup(data, dir_path):
+    zip_path = data["downloaded_path"]
+    for path in (dir_path, zip_path):
+        if os.path.exists(path):
+            if os.path.isdir(path):
+                shutil.rmtree(path)
+            else:
+                os.remove(path)
+            print(f"Удалён {path}")
+
 @dag(
     dag_id='g4_konovalov_daniil_s1_dag',
     description='my first dag for s1',
@@ -43,87 +133,11 @@ default_args = {
     default_args=default_args,
 )
 def xz():
-    @task
-    def extract():
-        context = get_current_context()
-        execution_date = context["ds_nodash"][:6]
-        file_name = f"{execution_date}-citibike-tripdata.zip"
-
-        s3_hook_yandex = S3Hook(aws_conn_id="yandex_s3")
-
-        check_if_month_exists(file_name, s3_hook_yandex, BUCKET_NAME_SOURCE)
-
-        downloaded_path = s3_hook_yandex.download_file(
-            key=file_name,
-            bucket_name=BUCKET_NAME_SOURCE,
-            local_path=SHARED_BASE_DIR,
-        )
-        print(execution_date[:6])
-        return {
-            "downloaded_path": downloaded_path,
-            "file_name": file_name,
-        }
-
-    @task()
-    def unzip(data):
-        zip_path = data["downloaded_path"]
-        file_name = data["file_name"]
-
-        base_dir = os.path.dirname(zip_path)
-        extract_dir = os.path.join(
-            base_dir,
-            file_name.replace(".zip", "")
-        )
-
-        os.makedirs(extract_dir, exist_ok=True)
-
-        with zipfile.ZipFile(zip_path, "r") as z:
-            z.extractall(extract_dir)
-
-        return extract_dir
-
-    @task()
-    def load(extract_dir):
-        csv_files = []
-
-        for root, _, files in os.walk(extract_dir):
-            for f in files:
-                if CSV_RE.match(f):
-                    csv_files.append(os.path.join(root, f))
-
-        s3_hook_target = S3Hook(aws_conn_id='minio')
-        context = get_current_context()
-        execution_date = context["ds_nodash"][:6]
-        keys = []
-
-        for idx, file in enumerate(sorted(csv_files)):
-            part = f"{idx:02d}"
-            key = f"raw/citibike_data/{execution_date}/{execution_date}-citibike-tripdata-part{part}.csv"
-
-            if check_if_month_exists(key, s3_hook_target, BUCKET_NAME_TARGET):
-                print(f"Файл {file} уже загружен")
-
-            else:
-                s3_hook_target.load_file(
-                    filename=file,
-                    key=key,
-                    bucket_name=BUCKET_NAME_TARGET,
-                )
-                keys.append(key)
-
-
-                print(f"Файл успешно загружен: {file} -> {key}")
-
-    @task(trigger_rule="all_done")
-    def cleanup(data, dir_path):
-        zip_path = data["downloaded_path"]
-        for path in (dir_path, zip_path):
-            if os.path.exists(path):
-                if os.path.isdir(path):
-                    shutil.rmtree(path)
-                else:
-                    os.remove(path)
-                print(f"Удалён {path}")
+    list_keys = S3ListOperator(
+        task_id="list_keys",
+        bucket=bucket_name,
+        prefix=PREFIX,
+    )
 
     data = extract()
     extract_root = unzip(data)
